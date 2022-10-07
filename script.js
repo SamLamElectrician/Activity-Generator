307a480428ee1ae5567e0389f829b2d661b361cd
SammyLam <sammylam_1@hotmail.com>
2022-10-07 11:24:30 -0400

    popup modal finished n stylized

--- script.js
+++ script.js
@@ -1,182 +1,168 @@
-
-const boredApp = {}
+const boredApp = {};
 //url endpoint for random and start point for search params
-boredApp.url = "http://www.boredapi.com/api/activity"
+boredApp.url = "http://www.boredapi.com/api/activity";
 
-boredApp.generateButton = document.getElementById("generate")
-boredApp.randomButton = document.getElementById("random")
-boredApp.displayActivityDiv = document.getElementById("displayActivity")
+boredApp.generateButton = document.getElementById("generate");
+boredApp.randomButton = document.getElementById("random");
+boredApp.displayActivityDiv = document.getElementById("displayActivity");
 
 //event listener for buttons
 //both lead to display() to display
-boredApp.generateButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    boredApp.getUserData()
-
-})
+boredApp.generateButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  boredApp.getUserData();
+});
 
 boredApp.randomButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    boredApp.random()
-})
+  e.preventDefault();
+  boredApp.random();
+});
 
 //gets data for random activity/button
 boredApp.random = () => {
-    fetch("http://www.boredapi.com/api/activity")
-        .then((res) => {
-            return res.json();
-        })
-        .then((jsonResponse) => {
-            const activity = jsonResponse.activity
-            console.log(jsonResponse)
-            boredApp.display(activity)
-
-        })
-}
+  fetch("http://www.boredapi.com/api/activity")
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonResponse) => {
+      const activity = jsonResponse.activity;
+      console.log(jsonResponse);
+      boredApp.display(activity);
+    });
+};
 
 //drycode to present items
 boredApp.display = (activity) => {
-    const p = document.getElementById('activityP')
-    p.innerHTML = `&#128150; &#128150; &#128150; ${activity} together  &#128150; &#128150; &#128150;`
-}
+  const p = document.getElementById("activityP");
+  p.innerHTML = `&#128150; &#128150; &#128150; ${activity} together  &#128150; &#128150; &#128150;`;
+};
 
 //error code for unmatched params
 boredApp.tryAgain = (activity) => {
-    const p = document.getElementById('activityP')
-    p.innerHTML = ` &#128150; &#128150; &#128150; Try again &#128150; &#128150; &#128150;`
-}
-
+  const p = document.getElementById("activityP");
+  p.innerHTML = ` &#128150; &#128150; &#128150; Try again &#128150; &#128150; &#128150;`;
+};
 
 //search generator for generate button
 boredApp.getUserData = () => {
-    const activityVal = document.getElementById('people').value
-    const maxPrice = document.getElementById("price").value
-    const url = new URL(boredApp.url)
-    //defines search params
-    url.search = new URLSearchParams({
-        "type": activityVal, //type refers to style of activity 
-        "minprice": 0,
-        "maxprice": maxPrice,
+  const activityVal = document.getElementById("people").value;
+  const maxPrice = document.getElementById("price").value;
+  const url = new URL(boredApp.url);
+  //defines search params
+  url.search = new URLSearchParams({
+    type: activityVal, //type refers to style of activity
+    minprice: 0,
+    maxprice: maxPrice,
+  });
+
+  //uses search params to create endpoint to fetch data
+  console.log(url);
+  fetch(url)
+    .then((res) => {
+      return res.json();
     })
+    .then((jsonResponse) => {
+      const genActivity = jsonResponse.activity;
+      //if statement incase you get an undefined activity aka params dont match
+      if (genActivity === undefined) {
+        boredApp.tryAgain();
+      } else {
+        boredApp.display(genActivity);
+      }
+    });
+};
 
-    //uses search params to create endpoint to fetch data
-    console.log(url)
-    fetch(url)
-        .then((res) => {
-            return res.json();
-        })
-        .then((jsonResponse) => {
-            const genActivity = jsonResponse.activity
-            //if statement incase you get an undefined activity aka params dont match
-            if (genActivity === undefined) {
-                boredApp.tryAgain()
-            } else {
-                boredApp.display(genActivity)
-            }
-
-        })
-}
-
-boredApp.init = () => {
-}
+boredApp.init = () => {};
 //do we need init for event listeners??
 
-boredApp.init()
+boredApp.init();
 
 //button random completed
 //add generate params
 //add stylings
-//add weather last 
+//add weather last
 
+const weatherApp = {};
 
-const weatherApp = {}
+weatherApp.form = document.querySelector("form");
+weatherApp.url = "https://api.openweathermap.org/data/2.5/weather";
+weatherApp.apiKey = "46b73165b95ad3c8b3d3ecd596052a25";
 
-
-
-weatherApp.form = document.querySelector('form');
-weatherApp.url = "https://api.openweathermap.org/data/2.5/weather"
-weatherApp.apiKey = "46b73165b95ad3c8b3d3ecd596052a25"
-
-const url = new URL(weatherApp.url)
+const url = new URL(weatherApp.url);
 
 weatherApp.searchCity = (cityName) => {
+  url.search = new URLSearchParams({
+    q: cityName,
+    appid: weatherApp.apiKey,
+    units: "metric",
+  });
+  weatherApp.fetchData(url);
+};
 
-    url.search = new URLSearchParams({
-        "q": cityName,
-        "appid": weatherApp.apiKey,
-        "units": "metric"
-    })
-    weatherApp.fetchData(url);
-}
-
-weatherApp.form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const city = document.getElementById("city").value;
-    weatherApp.searchCity(city);
-    console.log(city)
-
-})
-
+weatherApp.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const city = document.getElementById("city").value;
+  weatherApp.searchCity(city);
+  console.log(city);
+});
 
 weatherApp.fetchData = (url) =>
-    fetch(url)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            else {
-                throw new Error('Something went wrong');
-            }
-        })
-        .then((jsonResponse) => {
-
-            const weatherData = {
-                mainTemp: (jsonResponse.main.temp),
-                feelsLike: (jsonResponse.main.feels_like),
-                windSpeed: (jsonResponse.wind.speed),
-                cloudData: (jsonResponse.weather[0].main),
-                icon: (jsonResponse.weather[0].icon),
-                place: (jsonResponse.name)
-
-            }
-            weatherApp.displayWeather(weatherData)
-        })
-        .catch(weatherApp.wrongCity)
-
-
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((jsonResponse) => {
+      const weatherData = {
+        mainTemp: jsonResponse.main.temp,
+        feelsLike: jsonResponse.main.feels_like,
+        windSpeed: jsonResponse.wind.speed,
+        cloudData: jsonResponse.weather[0].main,
+        icon: jsonResponse.weather[0].icon,
+        place: jsonResponse.name,
+      };
+      weatherApp.displayWeather(weatherData);
+    })
+    .catch(weatherApp.wrongCity);
 
 weatherApp.displayWeather = (weather) => {
-    const weatherDisplay = document.querySelector('.weatherDisplay');
-    const city = document.querySelector('.city');
-    const sky = document.querySelector('.sky');
-    const temp = document.querySelector('.temp');
+  const weatherDisplay = document.querySelector(".weatherDisplay");
+  const city = document.querySelector(".city");
+  const sky = document.querySelector(".sky");
+  const temp = document.querySelector(".temp");
 
-    city.innerText = (`${weather.place}`);
-    temp.innerText = (`${Math.round(weather.feelsLike)} \u2103 `)
-    sky.innerText = (`${weather.cloudData}`);
+  city.innerText = `${weather.place}`;
+  temp.innerText = `${Math.round(weather.feelsLike)} \u2103 `;
+  sky.innerText = `${weather.cloudData}`;
 
+  // h2.innerText = (`${Math.round(weather.feelsLike)} \u2103 `)
+  const img = document.querySelector(".iconImage");
+  img.src = `http://openweathermap.org/img/wn/${weather.icon}.png`;
+  img.classList.add("icon");
+  // const h3 = document.createElement('h3')
+  // h3.innerText = (`${weather.cloudData}`)
 
-    // h2.innerText = (`${Math.round(weather.feelsLike)} \u2103 `)
-    const img = document.querySelector('.iconImage')
-    img.src = (`http://openweathermap.org/img/wn/${weather.icon}.png`)
-    img.classList.add("icon")
-    // const h3 = document.createElement('h3')
-    // h3.innerText = (`${weather.cloudData}`)
-
-    weatherDisplay.appendChild(img)
-
-}
+  weatherDisplay.appendChild(img);
+};
 
 weatherApp.wrongCity = () => {
-    document.getElementById("city").value = '';
-    alert('Wrong City');
-}
+  document.getElementById("city").value = "";
+  alert("Wrong City");
+};
 
+//popup Modal
 
-
-
-
-
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".popUp").style.display = "block";
+  }, 1000);
+});
+document.querySelector("#closePop").addEventListener("click", function () {
+  document.querySelector(".popUp").style.display = "none";
+});
 
 //STRETCH GOAL API
 // DISPLAY WEATHER IN TORONTO
